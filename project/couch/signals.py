01d040d1b2ff0da99149ca0f5a790ea3164e8d47
Andrew Cutler <macropin@gmail.com>
2015-08-04 20:59:57 +1000

    use zone_fully_saved_signal

--- project/couch/signals.py
+++ project/couch/signals.py
@@ -1,12 +1,13 @@
-from django.db.models.signals import post_save, post_delete
+from django.db.models.signals import post_delete
 from django.dispatch import receiver
 
 from dnsmanager.models import Zone
+from dnsmanager.signals import zone_fully_saved_signal
 
 from .handlers import save_zone, delete_zone
 
 
-@receiver(post_save, sender=Zone)
+@receiver(zone_fully_saved_signal)
 def zone_saved(sender, instance, created, **kwargs):
     return save_zone(instance, created)
 
