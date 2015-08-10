b74aedd0d8b50b33cd61c9dfdf11e0caf4aa6794
Andrew Cutler <macropin@gmail.com>
2015-08-10 13:25:12 +1000

    Improve admin

--- project/account/admin.py
+++ project/account/admin.py
@@ -3,16 +3,28 @@ from django.contrib import admin
 from .models import Domain, Organisation, Membership
 
 
+class DomainInline(admin.TabularInline):
+    model = Domain
+    extra = 0
+    can_delete = False
+
+
+class MembershipInline(admin.TabularInline):
+    model = Membership
+    extra = 0
+
+
 @admin.register(Domain)
 class DomainAdmin(admin.ModelAdmin):
     list_display = ('__unicode__', 'organisation', 'name',)
-    list_filter = ('organisation', 'name')
+    list_filter = ('organisation',)
 
 
 @admin.register(Organisation)
 class OrganisationAdmin(admin.ModelAdmin):
     list_display = ('__unicode__', 'name',)
     list_filter = ('name',)
+    inlines = (MembershipInline, DomainInline)
 
 
 @admin.register(Membership)
