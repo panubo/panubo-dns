73a1eb63cff3e8dc6cabcb562956fa86b5507c3c
Andrew Cutler <macropin@gmail.com>
2015-08-10 15:16:56 +1000

    Override the dnsmanager admin, adding support for qs limiting to match logged in users organisational permissions.

--- project/account/admin.py
+++ project/account/admin.py
@@ -2,6 +2,9 @@ from django.contrib import admin
 
 from .models import Domain, Organisation, Membership
 
+from dnsmanager.models import Zone
+from dnsmanager.admin import ZoneAdmin as ZoneAdminSuper
+
 
 class DomainInline(admin.TabularInline):
     model = Domain
@@ -14,10 +17,52 @@ class MembershipInline(admin.TabularInline):
     extra = 0
 
 
+class OrganisationListFilter(admin.SimpleListFilter):
+    """ Limit the List filter to valid options for the user """
+    title = 'Organisation'
+    parameter_name = 'organisation__id__exact'
+
+    def lookups(self, request, model_admin):
+        my_orgs = set([m.organisation for m in Membership.objects.filter(user=request.user)])
+        return [(m.pk, m.name) for m in my_orgs]
+
+    def queryset(self, request, queryset):
+        if self.value():
+            return queryset.filter(organisation_id=self.value())
+        else:
+            return queryset
+
+
+class DomainOrganisationListFilter(OrganisationListFilter):
+    """ Limit the List filter to valid options for the user """
+    parameter_name = 'domain_organisation__id__exact'
+
+    def queryset(self, request, queryset):
+        if self.value():
+            return queryset.filter(domain__organisation_id=self.value())
+        else:
+            return queryset
+
+
 @admin.register(Domain)
 class DomainAdmin(admin.ModelAdmin):
     list_display = ('__unicode__', 'organisation', 'name',)
-    list_filter = ('organisation',)
+    list_filter = (OrganisationListFilter,)
+
+    def get_queryset(self, request):
+        """ Limit results to qs """
+        qs = super(DomainAdmin, self).get_queryset(request)
+        if request.user.is_superuser:
+            return qs
+        return qs.filter(organisation__pk__in=request.user.organisations.values_list('organisation_id', flat=True))
+
+    def formfield_for_foreignkey(self, db_field, request=None, **kwargs):
+        """ Limit choices for 'organisation' field """
+        if db_field.name == 'organisation':
+            if not request.user.is_superuser:
+                kwargs["queryset"] = Organisation.objects.filter(
+                    pk__in=request.user.organisations.values_list('organisation_id', flat=True))
+        return super(DomainAdmin, self).formfield_for_foreignkey(db_field, request, **kwargs)
 
 
 @admin.register(Organisation)
@@ -31,3 +76,24 @@ class OrganisationAdmin(admin.ModelAdmin):
 class MembershipAdmin(admin.ModelAdmin):
     list_display = ('__unicode__', 'user', 'organisation')
     list_filter = ('user', 'organisation')
+
+
+admin.site.unregister(Zone)
+
+@admin.register(Zone)
+class ZoneAdmin(ZoneAdminSuper):
+    list_filter = (DomainOrganisationListFilter,)
+
+    def get_queryset(self, request):
+        """ Limit results to qs """
+        qs = super(ZoneAdmin, self).get_queryset(request)
+        if request.user.is_superuser:
+            return qs
+        return qs.filter(domain__organisation_id__in=request.user.organisations.values_list('organisation_id', flat=True))
+
+    def formfield_for_foreignkey(self, db_field, request=None, **kwargs):
+        """ Limit choices for 'domain' field """
+        if db_field.name == 'domain':
+            if not request.user.is_superuser:
+                kwargs["queryset"] = Domain.objects.filter(organisation_id__in=request.user.organisations.values_list('organisation_id', flat=True))
+        return super(ZoneAdmin, self).formfield_for_foreignkey(db_field, request, **kwargs)
