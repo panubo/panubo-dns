97e0c6f8c02051c6471ef2186fae13e6455223ce
Andrew Cutler <macropin@gmail.com>
2015-10-20 13:17:15 +1100

    Fix appname heading in password reset urls

--- project/urls.py
+++ project/urls.py
@@ -10,11 +10,13 @@ admin.site.site_title = 'Panubo DNS'
 admin.site.site_header = 'Panubo DNS'
 admin.site.index_title = 'Administration'
 
+extra_context = {'site_title': 'Panubo DNS', 'site_header': 'Panubo DNS',}
+
 urlpatterns = patterns('',
     url(r'^$', RedirectView.as_view(url=reverse_lazy('admin:login'), permanent=False)),
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^admin/password_reset/$', 'django.contrib.auth.views.password_reset', name='admin_password_reset'),
-    url(r'^accounts/', include('django.contrib.auth.urls')),
+    url(r'^admin/password_reset/$', 'django.contrib.auth.views.password_reset', {'extra_context': extra_context}, name='admin_password_reset'),
+    url(r'^accounts/', include('django.contrib.auth.urls'), {'extra_context': extra_context}),
     url(r'^api/', include('project.api.urls')),
     url(r'^zone/', include('dnsmanager.urls')),
 )
