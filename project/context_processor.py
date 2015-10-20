56ae6bbc686593f6a4f5a3a087ed672e9c2385c8
Andrew Cutler <macropin@gmail.com>
2015-10-21 09:48:20 +1100

    Use django.sites for registration email template, our context processor cannot be used directly.

--- project/context_processor.py
+++ project/context_processor.py
@@ -2,5 +2,5 @@ from django.conf import settings
 
 
 def app_name(request):
-    app_name = getattr(settings, 'APP_NAME', 'Unknown')
-    return {'APP_NAME': app_name}
+    name = getattr(settings, 'APP_NAME', 'Unknown')
+    return {'APP_NAME': name}
